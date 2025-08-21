16e2ef51954bf6edf886891bcbe9c63e65a0952d
BrandonFa1680 <brandonfabrizio0@gmail.com>
2025-08-21 09:12:47 -0500

    add exercises part 3.7-3.8

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 let phoneBook = [
     { 
@@ -32,6 +33,10 @@ const generateID = () => {
 }
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
+morgan.token('body', (req)=>JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
     response.json(phoneBook)
@@ -89,6 +94,7 @@ app.post('/api/persons', (request,response) => {
 
     response.json(person)
 })
+
 
 const PORT = 3001
 app.listen(PORT)    
